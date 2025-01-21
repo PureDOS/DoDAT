292c21ef98c6f0cce6a561fdd96f12b4e9958833
PureDOS <136184507+PureDOS@users.noreply.github.com>
2025-01-22 00:40:04 +0900

    Increment version to 0.3

--- DoDATGUI.cs
+++ DoDATGUI.cs
@@ -24,13 +24,13 @@ using System.Xml;
 
 [assembly: System.Reflection.AssemblyTitle("DoDATGUI")]
 [assembly: System.Reflection.AssemblyProduct("DoDATGUI")]
-[assembly: System.Reflection.AssemblyVersion("0.2.0.0")]
-[assembly: System.Reflection.AssemblyFileVersion("0.2.0.0")]
+[assembly: System.Reflection.AssemblyVersion("0.3.0.0")]
+[assembly: System.Reflection.AssemblyFileVersion("0.3.0.0")]
 [assembly: System.Runtime.InteropServices.ComVisible(false)]
 
 class DoDATGUI : Form
 {
-    const string DoDatExeVersionString = "DoDAT v0.2";
+    const string DoDatExeVersionString = "DoDAT v0.3";
 
     string OriginalText;
     private ComboBox cmbRunMode;
